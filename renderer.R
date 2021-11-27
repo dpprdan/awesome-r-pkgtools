925c6f6fe82d0bcede66bb212222ad6f12c17629
Daniel Possenriede <possenriede@gmail.com>
2021-11-27 21:19:02 +0100

    delete unnecessary files

--- renderer.R
+++
@@ -1,9 +0,0 @@
-
-# render README first
-xfun::Rscript_call(
-  rmarkdown::render,
-  list(input = 'README.Rmd')
-)
-
-# duplicate README.html as index.html
-fs::file_copy("README.html", "index.html", overwrite = TRUE)
